6346d4ba01c58552b4bb5eccd6a0b8023d527a33
yuwen41200 <yuwen41200@gmail.com>
2016-02-18 02:30:37 +0800

    Finished browser version detection.

--- script/main.js
+++ script/main.js
@@ -1,4 +1,11 @@
-jQuery(function($) {
+$(document).ready(function() {
+	"use strict";
+	if (window.navigator.userAgent.indexOf("MSIE") > 0) {
+		window.alert("Sorry, but your browser is outdated.\n" +
+			"You can only view the snapshot of the website\n" +
+			"unless you upgrade your browser to the latest version.");
+		window.location.replace("http://people.cs.nctu.edu.tw/~ywpu/img/snapshot.png");
+	}
 	$(window).on("scroll", function() {
 		if ($(this).scrollTop() > 0)
 			$("#scroll-top").show("fast");
